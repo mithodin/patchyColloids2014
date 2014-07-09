702c071cefef656b667b21eb070f38acb21f0677
Lucas Treffenstädt <lucas@treffenstaedt.de>
2014-07-09 11:53:53 +0200

    a little cleaning up of unneccessary stuff

--- parameters.h
+++ parameters.h
@@ -2,11 +2,9 @@
  * @file parameters.h
  * @brief Defines all the static parameters for the simulation.
  */
-//#import <pthread.h>
 
 //All the parameters
 extern const double U0;
 extern const double M1; //m1 is always 1
 extern const double delta;
 extern const double sigma;
-//extern pthread_mutex_t mtxRandom;
--- random.h
+++
@@ -1,5 +0,0 @@
-/**
- * @file random.h
- * @brief file to import the rng headers
- */
-#import "dSFMT/dSFMT.h"
