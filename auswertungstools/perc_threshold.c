1a38bc6dd67bb89ff4ccdee2feb6989f181a0438
Lucas Treffenstädt <lucas@treffenstaedt.de>
2014-07-27 21:25:13 +0200

    still not fixed. continue this at home.

--- auswertungstools/perc_threshold.c
+++ auswertungstools/perc_threshold.c
@@ -5,23 +5,26 @@
 typedef enum {READNUM,SKIPLINE,READPOS,END} state;
 typedef enum {THREEPATCH,TWOPATCH} species;
 
-const double binheight = 1.0;
+const double binheight = 2.0;
 const double height = 200;
 const double width = 100;
 double *pbond;
 int N = 0;
-state file_state = READNUM;
 double *x=NULL;
 double *z=NULL;
 double *a=NULL;
+int *bin=NULL;
 species *sp=NULL;
 int *totalpatches = NULL;
-int frames = 0;
 const double sigma = 1.0;
 const double delta = 0.11965683746373795115;
 
 int bin_index(double z){
 	return (int)(floor(z/binheight));
+}
+
+double distance(double x1, double z1, double x2, double z2){
+	return sqrt(pow(x1-x2,2)+pow(z1-z2,2));
 }
 
 int patches(int i){
@@ -33,7 +36,7 @@ int patches(int i){
 			return 2;
 			break;
 		default:
-			printf("Error!\n");
+			printf("Error! sp[i] is not valid!\n");
 			exit(1);
 	}
 }
@@ -63,22 +66,36 @@ double patchPositionZ(int k, int i){
 int bonds(int k, int l){
 	int i = 0;
 	int j = 0;
-	double d = sqrt(pow(x[k]-x[l],2)+pow(z[k]-z[l],2));
-	if(d > sigma+delta) return 0;
-	for(i = 0; i < patches(k); i++){
-		for( j = 0; j < patches(l); ++j ){
-			double p1X = patchPositionX(k,i);
-			double p1Z = patchPositionZ(k,i);
-			double p2X = patchPositionX(l,j);
-			double p2Z = patchPositionZ(l,j);
-				
-			d = sqrt((p1X-p2X)*(p1X-p2X)+(p1Z-p2Z)*(p1Z-p2Z));
-			if( d <= delta ){
-				return 1;
+	double d = distance(x[k],z[k],x[l],z[l]);
+	if(d > sigma+delta){
+		return 0;
+	}else if(d < sigma){
+		printf("too close! %f\n",d);
+	}else{
+		for(i = 0; i < patches(k); i++){
+			for( j = 0; j < patches(l); ++j ){
+				double p1X = patchPositionX(k,i);
+				double p1Z = patchPositionZ(k,i);
+				double p2X = patchPositionX(l,j);
+				double p2Z = patchPositionZ(l,j);
+					
+				d = distance(p1X,p1Z,p2X,p2Z);
+				if( d <= delta ){
+					return 1;
+				}
 			}
 		}
 	}
 	return 0;
+}
+
+int protected_index(int max, int index){
+	if(index>=max){
+		printf("index %d out of bounds!\n",index);
+		exit(1);
+	}else{
+		return index;
+	}
 }
 
 int main(int argc, char **argv){
@@ -88,30 +105,44 @@ int main(int argc, char **argv){
 		totalpatches = (int *)calloc(bins,sizeof(int));
 		int i=0,j=0,tmp=0;
 		FILE *datei=fopen(argv[1],"r");
-		size_t len = 80;
-		char *zeile=calloc(80,sizeof(char));
+		size_t len = 60;
+		char *zeile=malloc(60*sizeof(char));
 		char sp0;
-		double dx,dz;
-		while(getline(&zeile,&len,datei) != -1){
+		state file_state = READNUM;
+		int frames = 0;
+		while((sp0=fgetc(datei))!=EOF){
+		//while(getline(&zeile,&len,datei) != -1){
+			ungetc(sp0,datei);
 			switch(file_state){
 				case READNUM:
-					sscanf(zeile,"%d",&N);
-					i=0;
+					tmp=fscanf(datei,"%d",&N);
+					if(tmp!=1){
+						printf("error! Could not read N\n");
+						exit(1);
+					}
+					free(bin);
 					free(x);
 					free(z);
 					free(a);
 					free(sp);
+					bin = (int*)calloc(N,sizeof(int));
 					x = (double*)calloc(N,sizeof(double));
 					z = (double*)calloc(N,sizeof(double));
 					a = (double*)calloc(N,sizeof(double));
 					sp = (species*)calloc(N,sizeof(species));
+					i=0;
 					file_state=SKIPLINE;
 					break;
 				case SKIPLINE:
+					fgets(zeile,len,datei);
 					file_state = READPOS;
 					break;
 				case READPOS:
-					sscanf(zeile,"%[CN] %lf %lf %lf",&sp0,&x[i],&z[i],&a[i]);
+					tmp=fscanf(datei,"%[CN] %lf %lf %lf",&sp0,&(x[i]),&(z[i]),&(a[i]));
+					if(tmp!=4){
+						printf("error! Only %d variables read by fscanf.\n",tmp);
+						exit(1);
+					}
 					switch(sp0){
 						case 'C':
 							sp[i]=THREEPATCH;
@@ -123,11 +154,20 @@ int main(int argc, char **argv){
 							printf("Error! Wrong Species!\n");
 							exit(1);
 					}
-					totalpatches[bin_index(z[i])]+=patches(sp[i]);
+					bin[i]=bin_index(z[i]);
+					totalpatches[bin[i]]+=patches(i);
+					if(totalpatches[bin[i]]>10000){
+						printf("%d in bin %d\n",totalpatches[bin[i]],bin[i]);
+					}else if(totalpatches[bin[i]]==0){
+						printf("%d in bin %d\n",totalpatches[bin[i]],bin[i]);
+					}
 					for(j=0;j<i;++j){
 						tmp=bonds(i,j);
-						pbond[bin_index(z[i])]+=tmp;
-						pbond[bin_index(z[j])]+=tmp;
+						pbond[bin[i]]+=tmp;
+						pbond[bin[j]]+=tmp;
+						/*if(totalpatches[bin[j]]==0){
+							printf("j:%d i:%d\n",j,i);
+						} */
 					}
 					++i;
 					if(i==N){
@@ -141,11 +181,13 @@ int main(int argc, char **argv){
 					break;
 			}
 		}
-		//free(zeile);
 		fclose(datei);
+		//free(zeile);
 		for(i=0;i<bins;++i){
 			if(totalpatches[i]>0){
 				pbond[i]/=totalpatches[i];
+			}else{
+				if(pbond[i]!=0) printf("Something's weird in bin %d.\n%f bonds, %d patches.\n",i,pbond[i],totalpatches[i]);
 			}
 		}
 
@@ -155,7 +197,7 @@ int main(int argc, char **argv){
 			fprintf(output,"%d\t%.2f\t%.5e\n",i,(i+0.5)*binheight,pbond[i]);
 		}
 		fclose(output);
-		//free(pbond);
+		free(pbond);
 		free(totalpatches);
 	}else{
 		printf("Give me a filename!\n");
